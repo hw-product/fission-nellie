7170dcfbc3e2588ec69cc5375f27594ba60b859a
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-14 08:29:41 -0800

    Update message validity check. Cleanup process artifact and stub where we should be broadcasting

--- lib/fission-nellie/trent.rb
+++ lib/fission-nellie/trent.rb
@@ -8,10 +8,20 @@ module Fission
       end
 
       def execute(message)
-        File.open('/tmp/fubar', 'w') do |file|
-          file.puts "DONE!"
+        payload = unpack(message)
+        debug "Cleanup of nellie generated process - #{payload[:process_notification]}"
+        p_lock = process_manager.lock(payload[:process_notification])
+        %w(stdout stderr).each do |k|
+          p_lock[:process].io.send(k).rewind
+          debug "#{k}<#{payload[:process_notification]}>: #{p_lock[:process].io.send(k).read}"
         end
-        debug "Wrote final message for completion"
+        process_manager.unlock(p_lock)
+        process_manager.delete(payload[:process_notification])
+        if(payload[:nellie_commands] && !payload[:nellie_commands].empty?)
+          debug "Process cleanup is not final process for payload. No notifications."
+        else
+          debug "This payload is complete! Who the hell do I tell!?"
+        end
       end
     end
   end
