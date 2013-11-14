a7ea4ca4ff94a405c12c57ffa1367a480889231d
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-14 08:30:23 -0800

    Complete support for optional JSON nellie contents

--- lib/fission-nellie/banks.rb
+++ lib/fission-nellie/banks.rb
@@ -6,29 +6,74 @@ module Fission
 
       def valid?(message)
         m = unpack(message)
-        m[:repository]
-      end
-
-      def log_file(*args)
-        File.join('/tmp', *args)
+        if(m[:repository])
+          if(m[:process_notification])
+            m[:nellie_commands] && !m[:nellie_commands].empty?
+          else
+            true
+          end
+        end
       end
 
       def execute(message)
-        m = unpack(message)
+        payload = unpack(message)
+        process_pid = nil
+        command = nil
         debug "Processing message for testing"
-        if(File.exists?(test_path = File.join(m[:repository][:path], SCRIPT_NAME)))
-          debug "Running test at path: #{test_path}"
-          process_pid = Celluloid.uuid
-          process_manager.process(process_pid, message[:source], ['/bin/bash', test_path]) do |proc|
-            proc.cwd = File.dirname(test_path)
-#            proc.io.stdout = Tempfile.new(log_file(process_pid, 'stdout'))
-#            proc.io.stderr = Tempfile.new(log_file(process_pid, 'stderr'))
-#            proc.detach = true
-            proc.start
+        test_path = File.join(payload[:repository][:path], SCRIPT_NAME)
+
+        unless(payload[:nellie_commands])
+          if(File.exists?(test_path))
+            debug "Running test at path: #{test_path}"
+            begin
+              json = JSON.load(File.read(test_path))
+              debug 'Nellie file is JSON. Populating commands into payload and tossing back to the queue.'
+              payload[:nellie_commands] = json['commands']
+            rescue
+              debug 'Looks like that wasn\'t JSON. Lets just execute it!'
+              command = File.executable?(test_path) ? test_path : "/bin/bash #{test_path}"
+            end
+          else
+            abort "No nellie file found! (checked: #{test_path})"
           end
-          debug "Process left running with process id of: #{process_pid}"
-        else
-          debug "No test file found for processing (#{test_path})"
+        end
+        if(payload[:nellie_commands])
+          command = payload[:nellie_commands].shift
+        end
+        process_pid = run_process(command,
+          :source => message[:source],
+          :payload => payload,
+          :cwd => payload[:repository][:path]
+        )
+        debug "Process left running with process id of: #{process_pid}"
+      end
+
+      def run_script(test_path, source, payload)
+        run_process("/bin/bash #{test_path}",
+          :source => source,
+          :payload => payload,
+          :cwd => File.dirname(test_path)
+        )
+        process_pid = run_process(message, '/bin/bash', test_path)
+      end
+
+      def run_process(command, pack={})
+        process_pid = Celluloid.uuid
+        cwd = pack.delete(:cwd) || '/tmp'
+        stdout_log = process_manager.create_io_tmp(process_pid, 'stdout')
+        stderr_log = process_manager.create_io_tmp(process_pid, 'stderr')
+        process_manager.process(process_pid, command, pack) do |proc|
+          proc.cwd = cwd
+          proc.io.stdout = stdout_log
+          proc.io.stderr = stderr_log
+          proc.start
+        end
+        process_pid
+      end
+
+      def run_json(message, json)
+        json['commands'].each do |command|
+          run_process(message, *command.split(' '))
         end
       end
 
