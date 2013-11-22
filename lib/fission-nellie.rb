167b575ab8a0cb7a9d79938e3cd09082ce27502e
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-22 09:30:22 -0800

    Remove internal validator auto load

--- lib/fission-nellie.rb
+++ lib/fission-nellie.rb
@@ -3,7 +3,3 @@ require 'fission-nellie/version'
 require 'fission-nellie/banks'
 require 'fission-nellie/bly'
 require 'fission-nellie/trent'
-
-Dir.glob(File.join(File.dirname(__FILE__), 'fission-nellie', 'validations', '*.rb')).each do |path|
-  require "fission-nellie/validations/#{File.basename(path).sub('.rb', '')}"
-end
