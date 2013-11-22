1fe4c4c8137b66550e2ef862b47503605b9b9480
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-22 09:31:58 -0800

    Add more useful information output. Check on exit code for success

--- lib/fission-nellie/trent.rb
+++ lib/fission-nellie/trent.rb
@@ -6,29 +6,26 @@ module Fission
 
       def valid?(message)
         m = unpack(message)
-        m[:process_notification]
+        m[:data][:process_notification]
       end
 
       def execute(message)
         payload = unpack(message)
-        debug "Cleanup of nellie generated process - #{payload[:process_notification]}"
-        p_lock = process_manager.lock(payload[:process_notification])
+        debug "Cleanup of nellie generated process - #{payload[:data][:process_notification]}"
+        p_lock = process_manager.lock(payload[:data][:process_notification])
         %w(stdout stderr).each do |k|
           p_lock[:process].io.send(k).rewind
-          debug "#{k}<#{payload[:process_notification]}>: #{p_lock[:process].io.send(k).read}"
+          debug "#{k}<#{payload[:data][:process_notification]}>: #{p_lock[:process].io.send(k).read}"
         end
-        successful = p_lock[:process].crashed?
+        successful = p_lock[:process].exit_code == 0
         process_manager.unlock(p_lock)
-        process_manager.delete(payload[:process_notification])
+        process_manager.delete(payload[:data][:process_notification])
         if(successful)
-          if(payload[:nellie_commands] && !payload[:nellie_commands].empty?)
-            debug "Process cleanup is not final process for payload. No notifications."
-          else
-            [:nellie_commands, :process_notification].each{|key| payload.delete(key) }
-            completed(payload, message)
-          end
+          payload[:data].delete(:process_notification)
+          forward(payload)
         else
-          error "Process failed! Send notification at what?"
+          error "Nellie process failed! Process ID: #{payload[:data][:process_notification]}"
+          debug "Payload needs to be transmitted to `:finalizer`"
         end
       end
     end
