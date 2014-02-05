a13d129e92a448dcbe0596ba72a1594dcb1f67ee
Chris Roberts <chrisroberts.code@gmail.com>
2014-02-04 16:20:58 -0800

    Generally a single `=` is more than enough for variable assignment

--- lib/fission-nellie/banks.rb
+++ lib/fission-nellie/banks.rb
@@ -92,7 +92,7 @@ module Fission
       def run_process(command, pack={})
         process_pid = Celluloid.uuid
         cwd = pack.delete(:cwd) || '/tmp'
-        env = = pack.delete(:environment)
+        env = pack.delete(:environment)
         stdout_log = process_manager.create_io_tmp(process_pid, 'stdout')
         stderr_log = process_manager.create_io_tmp(process_pid, 'stderr')
         process_manager.process(process_pid, command, pack) do |proc|
