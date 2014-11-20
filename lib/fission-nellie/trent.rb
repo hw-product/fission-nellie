23b0efb925725734a4ba8acd93b65cc93a1bd963
Anthony Goddard <anthony@anthonygoddard.com>
2014-11-20 16:34:02 -0700

    only keep last 200 lines for logs, move them to asset store

--- lib/fission-nellie/trent.rb
+++ lib/fission-nellie/trent.rb
@@ -28,21 +28,26 @@ module Fission
           p_lock = process_manager.lock(payload[:data][:process_notification])
           logs = {}
           %w(stdout stderr).each do |k|
-            p_lock[:process].io.send(k).rewind
-            logs[k] = p_lock[:process].io.send(k).read
-            debug "#{k}<#{payload[:data][:process_notification]}>: #{logs[k]}"
+            key = "nellie-output/#{payload[:data][:process_notification]}/#{k}"
+            io = p_lock[:process].io.send(k)
+            io.rewind
+            @object_store.put(key, io)
+            logs[k] = key
+            position = io.size < 200 ? 0 : io.size - 200
+            io.seek(position)
+            debug "#{k}<#{payload[:data][:process_notification]}>: #{io.read}"
           end
           successful = p_lock[:process].exit_code == 0
           process_manager.unlock(p_lock)
           process_manager.delete(payload[:data][:process_notification])
           payload[:data].delete(:process_notification)
+          payload.set(:data, :nellie, :logs, logs)
           if(successful)
             payload.set(:data, :nellie, :status, 'ok')
             forward(payload)
           else
             error "Nellie process failed! Process ID: #{payload[:data][:process_notification]}"
             payload.set(:data, :nellie, :status, 'fail')
-            payload.set(:data, :nellie, :logs, logs)
             job_completed('nellie', payload, message)
           end
         end
